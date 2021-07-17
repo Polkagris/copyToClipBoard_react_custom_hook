e118349ef5c675228cae615a300c2125ba6e9ff9
unknown <arne.n.bjerkaas@gmail.com>
2021-07-17 10:42:09 +0200

    Add timer

--- src/components/CopyButton.jsx
+++ src/components/CopyButton.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { useCopyToClipboard } from "../hooks/useCopyToClipboard";
 
 const CopyButton = ({ textToCopy }) => {
-  const [isCopied, handleCopy] = useCopyToClipboard();
-  console.log("isCopied: ", isCopied);
+  const [isCopied, handleCopy] = useCopyToClipboard(2000);
 
-  return <button onClick={() => handleCopy(textToCopy)}>copy</button>;
+  return (
+    <button disabled={isCopied} onClick={() => handleCopy(textToCopy)}>
+      {isCopied ? "copied" : "copy"}
+    </button>
+  );
 };
 
 export default CopyButton;
--- src/hooks/useCopyToClipboard.js
+++ src/hooks/useCopyToClipboard.js
@@ -1,19 +1,19 @@
 import { useState, useEffect } from "react";
 import copy from "copy-to-clipboard";
 
-export const useCopyToClipboard = (resetTimer = null, inputText) => {
+export const useCopyToClipboard = (timeDisabled = null) => {
   const [isCopied, setCopied] = useState(false);
 
-  const handleCopy = (input) => {
-    if (typeof input === "string" || typeof input === "number") {
-      // copy
-      copy(input.toString());
-      setCopied(true);
-    } else {
-      setCopied(false);
-      console.error(`Cannot copy type of ${typeof input}`);
-    }
+  const handleCopy = (textToCopy) => {
+    copy(textToCopy.toString());
+    setCopied(true);
   };
+
+  useEffect(() => {
+    if (!isCopied) return;
+    let timeOut = window.setTimeout(() => setCopied(false), timeDisabled);
+    return () => window.clearTimeout(timeOut);
+  }, [isCopied]);
 
   return [isCopied, handleCopy];
 };
